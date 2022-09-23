9023bba6bf03b7e69bc7bf057d5dddaa88711620
PanGeun <lifebook0809@gmail.com>
2022-09-23 16:29:40 +0900

    [feat] MeetController 생성
    - meet_main.do : MEET02.jsp로 이동

---
+++ meet-a-bwa/src/main/java/test/com/meet/controller/MeetController.java
@@ -0,0 +1,45 @@
+package test.com.meet.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MeemController
+ */
+@WebServlet("/meet_main.do")
+public class MeetController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MeetController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String sPath = request.getServletPath();
+		
+		if (sPath.equals("/meet_main.do")) {
+			request.getRequestDispatcher("views/meet/MEET02.jsp").forward(request, response);
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
