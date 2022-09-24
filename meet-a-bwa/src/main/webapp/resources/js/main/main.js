bcb87f019b3260d917186123660cfb04b387f7d2
wtw <qjspdl@hs.ac.kr>
2022-09-25 00:26:20 +0900

    [feat] main에서 모임 추천 섹션 통신 추가
    ajax로 값보내기 기능 구현

--- meet-a-bwa/src/main/webapp/resources/js/main/main.js
+++ meet-a-bwa/src/main/webapp/resources/js/main/main.js
@@ -1,8 +1,21 @@
 $(document).ready(function() {
 
-//	$.ajax({
-//		url : "/meet-a-bwa/main_init.do"
-//	});
+	$.ajax({
+		url : "/meet-a-bwa/main_init.do",
+		type : "POST",
+		dataType : 'json',
+		data : {
+			isLogin : false, // 추후에 쿠키로 값 가져올거임
+			userInterest : "",
+			userRegion : ""
+		},
+		success : function(result) {
+			console.log(result);    
+		},
+		error : function(request, status, error) {
+		 	console.log(error);
+		 }
+	});
 	
     $("#plusImg").click(function(){
         $("#fold_tag").removeClass("blind");
@@ -20,5 +33,9 @@ $(document).ready(function() {
         $(".tagItem").removeClass("check");
         $(e.target).addClass("check");
     })
+    
+ 	$(".btn-cancel").click(function(){
+ 		$(".logout-layer").addClass("blind");
+ 	});
     
 });
