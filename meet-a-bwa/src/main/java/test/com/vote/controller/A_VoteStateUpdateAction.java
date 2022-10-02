a4aa2cc571562ed354ec74eba9582baadceb1337
wtw <qjspdl@hs.ac.kr>
2022-10-02 23:19:32 +0900

    [ui] 이벤트 생성/수정/확인

--- meet-a-bwa/src/main/java/test/com/vote/controller/A_VoteStateUpdateAction.java
+++ meet-a-bwa/src/main/java/test/com/vote/controller/A_VoteStateUpdateAction.java
@@ -12,15 +12,12 @@ import test.com.vote.model.VoteDAOImpl;
 public class A_VoteStateUpdateAction {
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String vote_no = request.getParameter("vote_no");
-		String meet_no = request.getParameter("meet_no");
-		
-		System.out.println("vote_no : " + vote_no);
-		System.out.println("meet_no : " + meet_no);
+		String activity_no = request.getParameter("activity_no");
 		
 		VoteDAO dao = new VoteDAOImpl();
 		int result = dao.update_voteState(vote_no);
 		if(result == 1) {
-			response.sendRedirect("/meet-a-bwa/meet-main.do?idx=" + meet_no);
+			response.sendRedirect("/meet-a-bwa/activity-main.do?idx=" + activity_no);
 		}
 	}
 }
